1cb3ba2db0b04fa0207c89e602214054236c8e7b
ayush7125 <155421615+ayush7125@users.noreply.github.com>
2025-06-25 23:30:05 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
@@ -11,6 +10,7 @@ export default defineConfig({
     }
   },
   preview: {
-    host: true
+    host: true,
+    allowedHosts: ['dcp-frontend.onrender.com']
   }
 })
